b3ce7a1392209ad4acb8869d3fc1914a009a39dc
Brandon Xu <brandon.xu@wsu.edu>
2024-04-23 02:47:07 -0700

    Add khaki color to color constants

--- lib/core/theme/color_style.dart
+++ lib/core/theme/color_style.dart
@@ -4,6 +4,7 @@ const raisinBlackColor = Color.fromRGBO(37, 35, 35, 1);
 const slateGreyColor = Color.fromRGBO(112, 121, 140, 1);
 const boneColor = Color.fromRGBO(218, 210, 188, 1);
 const isabellineColor = Color.fromRGBO(245, 241, 237, 1);
+const khakiColor = Color.fromRGBO(169, 153, 133, 1);
 const lightGreyColor = Colors.white70;
 const whiteColor = Colors.white;
 const blackColor = Colors.black;
