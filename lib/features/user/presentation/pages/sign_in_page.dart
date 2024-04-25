93d560eda3c136bb13de343553845ebb120ddc6d
Brandon Xu <brandon.xu@wsu.edu>
2024-04-25 01:03:50 -0700

    Modify sign in page

--- lib/features/user/presentation/pages/sign_in_page.dart
+++ lib/features/user/presentation/pages/sign_in_page.dart
@@ -18,72 +18,125 @@ class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      margin: const EdgeInsets.symmetric(horizontal: 14, vertical: 50),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const SizedBox(height: 10),
-            const FaIcon(FontAwesomeIcons.drumstickBite, size: 90),
-            Form(
-              key: _formKey,
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxHeight: MediaQuery.of(context).size.height,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 40),
+            child: Center(
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const Text('Login', style: Styles.title1),
-                  const SizedBox(height: 25),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        contentPadding:
-                            EdgeInsets.symmetric(vertical: 6, horizontal: 8),
-                        labelText: 'Username',
-                        helperText: ''),
-                    validator: (value) {
-                      if (!notEmptyCheck(value)) {
-                        return 'Please enter a username';
-                      }
-                      return null;
-                    },
+                  const SizedBox(height: 10),
+                  const Column(
+                    children: [
+                      FaIcon(FontAwesomeIcons.drumstickBite, size: 90),
+                      SizedBox(height: 20),
+                      Text(
+                        'Proteam',
+                        style: Styles.title1,
+                      )
+                    ],
                   ),
-                  const SizedBox(height: 5),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        contentPadding:
-                            EdgeInsets.symmetric(vertical: 6, horizontal: 8),
-                        labelText: 'Password',
-                        helperText: ''),
-                    validator: (value) {
-                      if (!notEmptyCheck(value)) {
-                        return 'Please enter a password';
-                      }
-                      return null;
-                    },
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            SizedBox(width: 5),
+                            Text('Let\'s sign you in.',
+                                style: Styles.headline1),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        TextFormField(
+                          scrollPadding: const EdgeInsets.only(bottom: 150),
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 6, horizontal: 8),
+                              labelText: 'Username',
+                              helperText: ''),
+                          validator: (value) {
+                            if (!notEmptyCheck(value)) {
+                              return 'Please enter a username';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 5),
+                        TextFormField(
+                          scrollPadding: const EdgeInsets.only(bottom: 220),
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 6, horizontal: 8),
+                              labelText: 'Password',
+                              helperText: ''),
+                          validator: (value) {
+                            if (!notEmptyCheck(value)) {
+                              return 'Please enter a password';
+                            }
+                            return null;
+                          },
+                        ),
+
+                        // Forgot password
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text('Forgot Password?'),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
+                  Column(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          if (_formKey.currentState!.validate()) {}
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: isabellineColor,
+                              borderRadius: BorderRadius.circular(12)),
+                          padding: const EdgeInsets.all(15),
+                          child: const Center(
+                              child: Text('Log in',
+                                  style: TextStyle(
+                                      color: blackColor,
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 22))),
+                        ),
+                      ),
+
+                      const SizedBox(height: 25),
+                      // Register option
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text('Don\'t have an account?'),
+                          GestureDetector(
+                              onTap: () {},
+                              child: const Text(
+                                ' Register',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ))
+                        ],
+                      )
+                    ],
+                  )
                 ],
               ),
             ),
-            GestureDetector(
-              onTap: () {
-                if (_formKey.currentState!.validate()) {}
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                    color: isabellineColor,
-                    borderRadius: BorderRadius.circular(12)),
-                padding: const EdgeInsets.all(15),
-                child: const Center(
-                    child: Text('Log in',
-                        style: TextStyle(
-                            color: blackColor,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24))),
-              ),
-            )
-          ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
